3a2b7a34f105bd5389338d6b1cf844fd486c1223
Jens Diemer <jens.diemer@boxine.de>
2022-02-03 16:48:49 +0100

    Don't call fix-code-style while publishing
    Code check is done in pytest run, since: https://github.com/boxine/django-huey-monitor/pull/74
    
    And there is a problem with 'fix-code-style', because of:
    https://github.com/akaihola/darker/issues/273 (black would like to access docker volumes and raise a
    permission error)

--- huey_monitor_tests/test_project/publish.py
+++ huey_monitor_tests/test_project/publish.py
@@ -20,6 +20,5 @@ def publish():
             $ poetry run publish
     """
     verbose_check_call('make', 'pytest')  # don't publish if tests fail
-    verbose_check_call('make', 'fix-code-style')  # don't publish if code style wrong
 
     poetry_publish(package_root=PACKAGE_ROOT, version=huey_monitor.__version__)
